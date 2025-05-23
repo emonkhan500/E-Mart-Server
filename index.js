e4a4b0b7cbfc97bd78206d89c676571816818ce3
emonkhan500 <khdilo47@gmail.com>
2025-05-23 21:44:50 +0600

    env added to server

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://E-Mart:OhUbIBBDsTOOiIAD@cluster0.tp2ab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tp2ab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
