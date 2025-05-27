15ddee6e97740c4775fe0cc4eb3df24c1bc9dc35
emonkhan500 <khdilo47@gmail.com>
2025-05-27 14:22:46 +0600

    user post api created

--- index.js
+++ index.js
@@ -30,7 +30,17 @@ const userCollection = client.db('eMart').collection('user')
 
 // Users API
 
+app.post('/users',async(req,res)=>{
+  const user = req.body
+  const query= {email:user.email}
+  const existingUser= await userCollection.findOne(query)
+  if(existingUser){
+    return res.send({message:'user already exist'})
+  }
+  const result = await userCollection.insertOne(user)
+  res.send(result)
 
+})
 
 
 
