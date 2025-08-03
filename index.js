03f10cf8cedd027d5fbed7b5320d9fd4e6de6975
emonkhan500 <khdilo47@gmail.com>
2025-08-03 11:06:39 +0600

    working on jwt api

--- index.js
+++ index.js
@@ -31,15 +31,15 @@ const userCollection = client.db('eMart').collection('user')
 
 
 // jwt
-app.post('/jwt',async(req,res)=>{
-  const user= req.body;
-  console.log(user);
-  res.send(user)
+app.post('jwt',async(req,res)=>{
+  const user = req.body;
+  const token= jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn: '1h'})
+  res.send({token})
 })
 
 // admin api
 
-app.patch('users/admin/:id', async(req,res)=>{
+app.patch('/users/admin/:id', async(req,res)=>{
   const id= req.params.id;
   const filter= {_id: new ObjectId(id)};
   const updateDoc={
