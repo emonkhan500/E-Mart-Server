ca6ca8fa3a119a17205471893d92596e1917d68a
emonkhan500 <khdilo47@gmail.com>
2025-09-13 10:23:57 +0600

    veryfy admin

--- index.js
+++ index.js
@@ -51,6 +51,17 @@ jwt.verify(token,process.env.ACCESS_TOKEN,(err,decoded)=>{
 })
 
 }
+// verify admin
+const verifyAdmin=async (req,res,next )=>{
+  const email= req.decoded.email
+  const query ={email:email}
+  const user= await userCollection.findOne(query)
+  const isAdmin= user?.role==='admin'
+  if(!isAdmin){
+    return res.status(403).send({message:'forbidden access'})
+  }
+next()
+}
 
 
 // admin api
@@ -98,7 +109,7 @@ app.post('/users',async(req,res)=>{
 
 })
 // Users Get
-app.get('/users',verifyToken, async(req,res)=>{
+app.get('/users',verifyToken,verifyAdmin, async(req,res)=>{
   const result = await userCollection.find().toArray()
   res.send(result)
 })
